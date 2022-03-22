4a48d8de5200cf3761eced497766fecfc050ea32
Tyrone Lagore <tyronelagore@gmail.com>
2022-03-21 21:30:12 -0700

    implement permute and substitude

--- sub-perm-network.py
+++ sub-perm-network.py
@@ -1,33 +1,36 @@
 
 from argparse import ArgumentError
+from tabnanny import verbose
 import time
 import random
 
 DEBUG = True
+VERBOSE = False
 
 def debug_print(*args, **kwargs):
     if DEBUG:
         print(f"| {time.strftime('%H:%M%p %Z on %b %d, %Y')} -- DEBUG:: ", end='')
         print(*args, **kwargs)
 
+def verbose_print(*args, **kwargs):
+    if VERBOSE:
+        debug_print(*args, **kwargs)
+
 class SBox:
-    def __init__(self, bits_size, n_rounds, random_key = time.time()):
-        if bits_size % 8 != 0:
-            raise ArgumentError("bits_size must be a multiple of 8")
+    BITS_SIZE = 16
 
-        self.bits_size = bits_size
-
+    def __init__(self, n_rounds, random_key = time.time()):
         random.seed(random_key)
         self.keys = []
 
         # generate random keys
-        [ self.keys.append(random.randint(0, 2**bits_size)) for _ in range(n_rounds+1) ]
+        [ self.keys.append(random.randint(0, 2**self.BITS_SIZE)) for _ in range(n_rounds+1) ]
 
         for idx, key in enumerate(self.keys):
             debug_print(f'key {idx+1}: {key:x}')
 
         # number of bytes that we can handle in 1 round
-        self.block_size = int(bits_size / 8)
+        self.block_size = int(self.BITS_SIZE / 8)
 
         self.n_rounds = n_rounds
 
@@ -94,7 +97,7 @@ class SBox:
         """ """
         for i in range(self.n_rounds):
             bits = self.round(bits, i)
-            debug_print(f"Encrypt: Round {i+1}: {bits:x}")
+            verbose_print(f"Encrypt: Round {i+1}: {bits:x}")
 
         bits = self._mix_key(bits, self.keys[self.n_rounds])
         return bits
@@ -119,7 +122,7 @@ class SBox:
                 bits = self._decrypt(bits)
 
             c += self._bits_to_str(bits)
-            c_bits = (c_bits << self.bits_size) | bits
+            c_bits = (c_bits << self.BITS_SIZE) | bits
 
             
         if encrypt:
@@ -135,7 +138,7 @@ class SBox:
 
         for i in range(self.n_rounds-1, -1, -1):
             bits = self.dnour(bits, i)
-            debug_print(f"Decrypt: Round {self.n_rounds - (i)}: {bits:x}")
+            verbose_print(f"Decrypt: Round {self.n_rounds - (i)}: {bits:x}")
 
         return bits
 
@@ -163,17 +166,43 @@ class SBox:
 
     def _substitute(self, message):
         """ """
+        ret_val = 0
+        mask = 0xF
+
+        for idx in range(0, int(self.BITS_SIZE/4)):
+            ret_val |= self.sub_lookup[(message & mask) >> (idx*4)]
+            mask <<= 4
+
         return message
 
     def _permute(self, message):
         """ """
-        return message
+        ret_val = 0
+        mask = 0x8000
+
+        # permute lookup is 1 indexed, so we go from [16 -> 1]
+        # leftmost bit is 1, rightmost bit is 16
+        for idx in range(1, self.BITS_SIZE+1):
+            # extract value of current 
+            curBit = (message & mask)
+
+            # only need to set the bit if it's 1
+            if curBit:
+                # need to shift it weird because the paper indexed with 1 as the leftmost bit for some reason
+                shift = self.BITS_SIZE-(self.perm_lookup[idx])
+                location = (1 << shift)
+                ret_val |= location
+                verbose_print(f"permute:: idx: {idx}, to:{(self.perm_lookup[idx])}, bit_val: {curBit}, shift: {shift}, mask: {mask:x}, location: {location:b} ret_val:{ret_val:b}")
+
+            mask >>= 1
+
+        return ret_val
 
     def _mix_key(self, bits, key):
         """ simple xor """
         return (bits ^ key)
 
 
-sbox = SBox(16, 4, 43)
+sbox = SBox(4, 43)
 encrypted = sbox.encrypt_decrypt("Junlin, I have created our sbox encryption implementation - woohoo!")
 sbox.encrypt_decrypt(encrypted, False)

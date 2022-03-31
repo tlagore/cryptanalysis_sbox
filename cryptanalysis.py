14a47aab57e13ddce047f5bbd09107c3585816dd
Tyrone Lagore <tyronelagore@gmail.com>
2022-03-31 11:46:30 -0700

    added encrypt/decrypt for regular 16 bit messages

--- cryptanalysis.py
+++ cryptanalysis.py
@@ -4,8 +4,11 @@ from subpermnetwork import SPN
 # 43 just lets us keep the random seed so we know expected keys
 spn = SPN(4, 43, [516,516,516,516,516])
 
-msg = 0xABCD
-print(f"Encrypting: {msg:x}")
-ciphertext = spn.encrypt(msg)
-decrypted = spn.decrypt(ciphertext)
-print(f"Decrypted: {decrypted:x}")
+msgs = [0xABCD, 0xDEF1, 0x1234, 0x5125, 0x3151]
+
+for msg in msgs: 
+    print(f"Encrypting: {msg:x}")
+    ciphertext = spn.encrypt(msg)
+    decrypted = spn.decrypt(ciphertext)
+    print(f"Decrypted: {decrypted:x}")
+    assert(msg == decrypted, f"Test fail! Expected: 0x{msg:x} but got 0x{decrypted:x}")
